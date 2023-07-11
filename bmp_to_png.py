5ff4bd8f41d322bb41a2f4bead1cf5a4674c786b
Atsushi <p.taki_atsu_0919@i.softbank.jp>
2023-07-11 15:30:05 +0900

    [add]画像の切り取り

--- bmp_to_png.py
+++ bmp_to_png.py
@@ -1,10 +1,18 @@
 import os
 from PIL import Image
 import glob
+import cv2
 
-filenames = glob.glob("data/*.bmp")
+filenames = glob.glob("/Users/takigawaatsushi/Documents/研究室/研究/data/EPMA/230710/*.bmp")
 
 for bmpname in filenames:
 	image = Image.open(bmpname)
 	pngname = os.path.splitext(bmpname)[0] + ".png"
 	image.save(pngname)
+
+	# 画像読み込み
+	img = cv2.imread(pngname)
+
+	# img[top : bottom, left : right]
+	cut_img = img[0 : 960, 0: 1280]
+	cv2.imwrite(os.path.splitext(pngname)[0] + "_cut.png", cut_img)
