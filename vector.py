6d6aead37e5b3fddacd6bce6798d1bb3c56220e9
Atsushi <p.taki_atsu_0919@i.softbank.jp>
2023-07-16 11:22:50 +0900

    [fix]x,yそれぞれで値を整える

--- vector.py
+++ vector.py
@@ -8,20 +8,29 @@ from skimage.filters import threshold_multiotsu
 import glob
 from sklearn.decomposition import PCA
 
+# labels = np.array(
+#         [
+#         0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 
+#         0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 
+#         0, 0, 2, 0, 0, 2, 0, 0, 2, 0, 0, 2, 0, 0, 2, 0, 0, 2, 0, 0, 2, 0, 0, 2, 0, 0, 2, 0, 0, 2, 
+#         0, 0, 2, 0, 0, 2, 0, 0, 2, 0, 0, 2, 0, 0, 2, 0, 0, 2, 0, 0, 2, 0, 0, 2, 0, 0, 2, 0, 0, 2,  
+#         0, 0, 3, 0, 0, 3, 0, 0, 3, 0, 0, 3, 0, 0, 3, 0, 0, 3, 0, 0, 3, 0, 0, 3, 0, 0, 3, 0, 0, 3, 
+#         0, 0, 3, 0, 0, 3, 0, 0, 3, 0, 0, 3, 0, 0, 3, 0, 0, 3, 0, 0, 3, 0, 0, 3, 0, 0, 3, 0, 0, 3, 
+#         ]
+#         )
+
 labels = np.array(
         [
-        1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 
-        1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 
-        2, 0, 0, 2, 0, 0, 2, 0, 0, 2, 0, 0, 2, 0, 0, 2, 0, 0, 2, 0, 0, 2, 0, 0, 2, 0, 0, 2, 0, 0, 
-        2, 0, 0, 2, 0, 0, 2, 0, 0, 2, 0, 0, 2, 0, 0, 2, 0, 0, 2, 0, 0, 2, 0, 0, 2, 0, 0, 2, 0, 0, 
+        1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 
+        1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 
         ]
         )
 
 
 pdnames = glob.glob("/Users/takigawaatsushi/Documents/研究室/研究/ph_analysis/output_for_vec/ph_results/*.pdgm")
 pdnames.sort()
-# for pdname in pdnames:
-#     print(pdname)
+for pdname in pdnames:
+    print(pdname)
 
 dimension = 1
 pds = [hc.PDList(pdname).dth_diagram(dimension) for pdname in pdnames]
@@ -36,10 +45,22 @@ pca.fit(pdvects)
 
 reduced = pca.transform(pdvects)
 
-plt.gca().set_aspect('equal')  # Set the aspect ratio of the figure
-plt.scatter(reduced[labels == 1, 0], reduced[labels == 1, 1], c="r")  # Show 1-labled data by "r"ed
-plt.scatter(reduced[labels == 2, 0], reduced[labels == 2, 1], c="b")  # Show 2-labled data by "b"lue
-#plt.scatter(reduced[labels == 3, 0], reduced[labels == 3, 1], c="g")  # Show 3-labled data by "b"lue
+# plt.gca().set_aspect('equal')  # Set the aspect ratio of the figure
+# plt.scatter(reduced[labels == 1, 0], reduced[labels == 1, 1], c="r")  # Show 1-labled data by "r"ed
+# plt.scatter(reduced[labels == 2, 0], reduced[labels == 2, 1], c="b")  # Show 2-labled data by "b"lue
+# #plt.scatter(reduced[labels == 3, 0], reduced[labels == 3, 1], c="g")  # Show 3-labled data by "b"lue
 
+x = []
+y = []
+for vec in reduced:
+    x.append(vec[0])
+    y.append(vec[1])
+x = x/max(x)
+y = y/max(y)
+
+plt.gca().set_aspect('equal')  # Set the aspect ratio of the figure
+plt.scatter(x[labels == 1], y[labels == 1], c="r")  # Show 1-labled data by "r"ed
+plt.scatter(x[labels == 2], y[labels == 2], c="b")  # Show 2-labled data by "b"lue
+plt.scatter(x[labels == 3], y[labels == 3], c="g")  # Show 3-labled data by "b"lue
 
 plt.savefig("result.png")
