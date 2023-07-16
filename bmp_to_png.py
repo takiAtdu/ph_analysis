c70622777d48991e3330279ccb6b23b64ed28ffc
Atsushi <p.taki_atsu_0919@i.softbank.jp>
2023-07-16 11:33:43 +0900

    [fix]出力png名を変更

--- bmp_to_png.py
+++ bmp_to_png.py
@@ -3,7 +3,7 @@ from PIL import Image
 import glob
 import cv2
 
-filenames = glob.glob("/Users/takigawaatsushi/Documents/研究室/研究/data/EPMA/230710/*.bmp")
+filenames = glob.glob("/Users/takigawaatsushi/Documents/研究室/研究/data/EPMA/230714/*.bmp")
 
 for bmpname in filenames:
 	image = Image.open(bmpname)
@@ -15,4 +15,4 @@ for bmpname in filenames:
 
 	# img[top : bottom, left : right]
 	cut_img = img[0 : 960, 0: 1280]
-	cv2.imwrite(os.path.splitext(pngname)[0] + "_cut.png", cut_img)
+	cv2.imwrite(pngname, cut_img)
