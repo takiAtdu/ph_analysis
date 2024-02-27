c91d9149e08294b080969ec0a55a70cb35a570f1
Atsushi <p.taki_atsu_0919@i.softbank.jp>
2024-02-27 14:15:07 +0900

    [modify]学習結果を

--- 07_lasso.py
+++ 07_lasso.py
@@ -10,6 +10,7 @@ import homcloud.interface as hc
 import glob
 import matplotlib.pyplot as plt
 import pickle
+import pandas as pd
 
 import common
 
@@ -39,19 +40,11 @@ lss.fit(X_train, y_train)
 
 save_dir = "output/lasso/"
 os.makedirs(save_dir, exist_ok=True)
-filename = phase + str(dimension) + ".sav"
+
+filename = "lasso_" + phase + str(dimension) + ".sav"
 pickle.dump(lss, open(save_dir+filename, 'wb'))
 
 y_pred = lss.predict(X_test)
-
-print('train score: ', lss.score(X_train, y_train))
-print('test score: ', lss.score(X_test, y_test))
-
-print('元の特徴量の数: ', X.shape[1])
-print('Lasso の特徴量: ', np.sum(lss.coef_ != 0))
-
-print('最小2乗誤差: ', mean_squared_error(y_test,y_pred))
-
 
 result_1600c24h = []
 result_1600c3h = []
@@ -74,22 +67,23 @@ for i in range(len(y_test)):
         result_ascast.append(y_pred[i])
 
 avg_1600c24h = sum(result_1600c24h)/len(result_1600c24h)
-print('1600c24hの平均値: ', avg_1600c24h)
-
 avg_1600c3h = sum(result_1600c3h)/len(result_1600c3h)
-print('1600c3hの平均値: ', avg_1600c3h)
-
 avg_1700c3h = sum(result_1700c3h)/len(result_1700c3h)
-print('1700c3hの平均値: ', avg_1700c3h)
-
 avg_1800c24h = sum(result_1800c24h)/len(result_1800c24h)
-print('1800c24hの平均値: ', avg_1800c24h)
-
 avg_1800c3h = sum(result_1800c3h)/len(result_1800c3h)
-print('1800c3hの平均値: ', avg_1800c3h)
-
 avg_ascast = sum(result_ascast)/len(result_ascast)
-print('ascastの平均値: ', avg_ascast)
+
+df_result = pd.DataFrame(data=[[lss.score(X_train, y_train)], [lss.score(X_test, y_test)], [X.shape[1]], 
+                            [np.sum(lss.coef_ != 0)], [mean_squared_error(y_test,y_pred)], 
+                            [avg_1600c3h], [avg_1600c24h], [avg_1700c3h], 
+                            [avg_1800c3h], [avg_1800c24h], [avg_ascast]], 
+                    index = ["train score", "test score", "元の特徴量の数", 
+                            "Lassoの特徴量", "最小2乗誤差", 
+                            "1600c3hの平均値", "1600c24hの平均値", "1700c3hの平均値", 
+                            "1800c3hの平均値", "1800c24hの平均値", "ascastの平均値"], 
+                    columns = ["値"])
+df_result.to_csv(save_dir + "lasso_" + phase + str(dimension) + ".csv")
+print(df_result)
 
 
 plt.scatter(y_pred, y_test, s = 20, c = "b")
@@ -102,5 +96,5 @@ plt.scatter(avg_ascast,   hv[5], s = 20, c = "r")
 plt.xlabel("predicted")
 plt.ylabel("measured")
 
-plt.savefig(save_dir + str(phase) + str(dimension) + ".png")
+plt.savefig(save_dir + "lasso_" + str(phase) + str(dimension) + ".png")
 # plt.show()
