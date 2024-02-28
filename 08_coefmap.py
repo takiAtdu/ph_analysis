237bbb99787a56fa890436dab55c1c0620e8c48d
Atsushi <p.taki_atsu_0919@i.softbank.jp>
2024-02-28 20:54:22 +0900

    [add]係数をnpy形式で保存

--- 08_coefmap.py
+++ 08_coefmap.py
@@ -2,6 +2,7 @@ import os
 import matplotlib.pyplot as plt
 import pickle
 import numpy as np
+import pandas as pd
 import homcloud.interface as hc
 
 import common
@@ -29,12 +30,13 @@ for i in range(bins):
 
 print("係数取得開始")
 
-model = "output/lasso/"+ phase + str(dimension) + ".sav"
+model = "output/lasso/lasso_"+ phase + str(dimension) + ".sav"
 lss = pickle.load(open(model, 'rb'))
 coef = lss.coef_
 
 count = 0
 effective_coef_dict = {}
+effective_coef_list = []
 for i in range(bins):
     for j in range(i+1):
         count += 1
@@ -47,10 +49,12 @@ for i in range(bins):
             y_dy = pd_range[0]+(pd_range[1]-pd_range[0])/bins*(i+1)
 
             effective_coef_dict[(x, y, x_dx, y_dy)] = lss.coef_[count-1]
+            effective_coef_list.append([x, y, x_dx, y_dy, lss.coef_[count-1]])
 
 # effective_coef_list = sorted(effective_coef_dict.items(), key = lambda coef : abs(coef[1]), reverse=True)
-effective_coef_list = sorted(effective_coef_dict.items(), key = lambda coef : coef[1], reverse=True)
-print(effective_coef_list)
+# effective_coef_list = sorted(effective_coef_dict.items(), key = lambda coef : coef[1], reverse=True)
+# print(effective_coef_list)
+
 
 
 print("係数取得終了")
@@ -61,6 +65,14 @@ print("描画開始")
 spec = hc.PIVectorizeSpec(pd_range, bins, sigma = sigma, weight = "none")
 spec.histogram_from_vector(coef).plot(colorbar={"type": "linear-midpoint", "midpoint": 0})
 
-save_dir = "output/coef_cmap/"
+save_dir = "output/coefmap/"
 os.makedirs(save_dir, exist_ok=True)
-plt.savefig(save_dir + phase + str(dimension) + ".png")
+save_path = save_dir + "coefmap_" + phase + str(dimension)
+plt.savefig(save_path + ".png")
+
+effective_coef_array = np.array(effective_coef_list)
+df_effective_coef = pd.DataFrame(data=effective_coef_array, columns=["x", "y", "x_dx", "y_dy", "coef"])
+df_effective_coef = df_effective_coef.sort_values("coef", ascending=False)
+print(df_effective_coef)
+effective_coef_array = df_effective_coef.to_numpy()
+np.save(save_path + ".npy", effective_coef_array)
